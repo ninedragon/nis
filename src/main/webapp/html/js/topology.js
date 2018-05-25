cb08378c61a88f7e77498d239987831383f4775c
ninedragon <shi_jin@hoperun.com>
2018-05-25 15:37:41 +0800

    代码提交

--- src/main/webapp/html/js/topology.js
+++ src/main/webapp/html/js/topology.js
@@ -214,7 +214,7 @@ function showTop(rowId){
 	 	        		 
 	 	        		 var branchBoxDifference = 100;//差值
 	 	        		 var branchBoxRelative = 100;//没有表箱的分支箱相对位置
-	 	        	   	 var gird = 250;
+	 	        	   	 var gird = 150;
 	 		        	 var tempXAll = null;
 	 	        		 
 	 	    	         var cabinetsX = branchBoxX;//出线柜X绝对位置
