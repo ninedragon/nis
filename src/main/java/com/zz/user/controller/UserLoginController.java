a344bd0fe27991006563f1dbc4067f04280e1b1e
Administrator <Administrator@USER-20180504PG>
2018-06-04 23:47:08 +0800

    默认登录画面及预警消息改造滚动条

--- src/main/java/com/zz/user/controller/UserLoginController.java
+++ src/main/java/com/zz/user/controller/UserLoginController.java
@@ -147,11 +147,11 @@ public class UserLoginController extends BaseController {
 			 */
 			LoggerUtils.fmtDebug(getClass(), "获取登录之前的URL:[%s]",url);
 			//如果登录之前没有地址，那么就跳转到首页。
-			if(StringUtils.isBlank(url)){
-				//登录成功修改成，直接倒转到实时监控页面
-				//url = request.getContextPath() + "/user/index.shtml";
-				url = request.getContextPath() + "/epu/allShowList.shtml";
-			}
+//			if(StringUtils.isBlank(url)){
+//				//登录成功修改成，直接倒转到实时监控页面
+//				url = request.getContextPath() + "/user/index.shtml";
+//			}
+			url = request.getContextPath() + "/epu/allShowList.shtml";
 			//跳转地址
 			resultMap.put("back_url", url);
 		/**
--- src/main/webapp/js/user.login.js
+++ src/main/webapp/js/user.login.js
@@ -1,6 +1,23 @@
 
+/**
+ * 获取项目根
+ * **/
+function getRootPath_web() {
+	 //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
+	 var curWwwPath = window.document.location.href;
+	 //获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
+	 var pathName = window.document.location.pathname;
+	 var pos = curWwwPath.indexOf(pathName);
+	 //获取主机地址，如： http://localhost:8083
+	 var localhostPaht = curWwwPath.substring(0, pos);
+	 //获取带"/"的项目名，如：/uimcardprj
+	 var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+	 return (localhostPaht + projectName);
+}  
+
 //换种方式获取，之前的方式在不同的环境下，有问题
 var baseUrl = $("script[baseUrl]").attr('baseUrl');
+baseUrl = baseUrl || getRootPath_web();
 /**退出*/
 function logout(){
 	var load = layer.load();
